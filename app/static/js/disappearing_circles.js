2b46a04fdfa90ef874482d0efa957bbf2b3314ab
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2024-12-26 22:43:50 -0600

    converted intervals to css animations

--- app/static/js/disappearing_circles.js
+++ app/static/js/disappearing_circles.js
@@ -2,34 +2,18 @@
 const storageName = 'dotGame-highScore-disappearingCircle';
 
 function gameStarter() {
-    startGame(game_action, setCircleDisappearingInterval, 8000, 1000);
+    startGame(game_action, 8000, setCircleDisappearingInterval, 1000);
 }
 
-function disappearCircle($circle) {
-    if ($circle.data('done') === 'true') {
-        return;
-    }
-    $circle.css('borderColor', 'transparent');
-    setTimeout(function() {
-        $circle.css('borderColor', 'white');
-    }, 500);
-}
 
 function setCircleDisappearingInterval($circle) {
-    disappearingInterval = setInterval(function() {
-        if ($circle.data('done') !== 'true') {
-            disappearCircle($circle);
-        }
-    }, 1500);
-    $circle.data('disappearingInterval', disappearingInterval);
+    $circle.addClass('disappear');
 }
 
-function resetCircle($circle) {
-    clearInterval($circle.data('disappearingInterval'));
+function resetCircle($circle) { //clearInterval($circle.data('disappearingInterval')); 
+    $circle.removeClass('disappear');
 }
 
 function clearCircle($circle) { $circle.remove(); }
 
-function clearCircles() {
-    $('.circle').each(function() { clearCircle($(this)); });
-}
+function clearCircles() { $('.circle').each(function() { clearCircle($(this)); }); }
--- app/static/js/disappearing_dot.js
+++ app/static/js/disappearing_dot.js
@@ -4,17 +4,9 @@ const storageName = 'dotGame-highScore-disappearingDot';
 let dotInterval;
 function gameStarter() {
     setTimeout(function() {
-        dotInterval = setInterval(disappearDot, 2500);
-    }, 1600);
+        $dot.addClass('disappear');
+    }, 3200);
     startGame(game_action, 4500, null, 500);
-}
-
-function disappearDot() {
-    $dot.css('backgroundColor', 'transparent');
-    console.log('Dot disappeared!');
-    setTimeout(function() {
-        $dot.css('backgroundColor', 'black');
-    }, 600);
 }
 
 function resetCircle(_) { return; }
