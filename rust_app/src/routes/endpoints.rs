4d59c0e4541f64f0280dae51ede5914935171763
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-01-07 14:32:39 -0600

    got score update working

--- rust_app/src/routes/endpoints.rs
+++ rust_app/src/routes/endpoints.rs
@@ -37,12 +37,12 @@ async fn validate_points(
     client_point_list: &Vec<Value>,
     score: u32,
 ) -> (bool, serde_json::Value) {
-    println!("Server point list");
-    println!("{:?}", server_point_list);
-    println!("Client point list");
-    println!("{:?}", client_point_list);
-    println!("Score: {}", score);
-    let latency_tolerance = 1000; // 1 second for now will adjust later
+    // println!("Server point list");
+    // println!("{:?}", server_point_list);
+    // println!("Client point list");
+    // println!("{:?}", client_point_list);
+    // println!("Score: {}", score);
+    let latency_tolerance = 500; // 1 second for now will adjust later
     let invalid_point_response = (false, json!({"error": "Invalid point time"}));
     // check if len of server list is less than len of client list
     if server_point_list.len() < client_point_list.len() || score > server_point_list.len() as u32 || score > client_point_list.len() as u32 {
@@ -50,43 +50,33 @@ async fn validate_points(
     }
     //loop through client_point_list
     for i in 0..client_point_list.len() {
-        if let Some(client_point) = client_point_list[i].as_object() {
-            // continue
-        } else {
-            // Handle the case where `as_object` returned `None`
-            eprintln!("client_point_list[{}] is not an object", i);
-        } // {'point_token', 'point_time'}
-        // find matching token in server_point_list
-        let server_point;
-        if let Some(server_point_obj) = server_point_list.iter().find(|&x| x.get("point_token") == client_point.get("point_token")) {
-            // check if point_time is within latency_tolerance
-            // point times are strings in iso format
-            let client_point_time;
-            let server_point_time;
-            if let Some(client_point_time_obj) = client_point.get("point_time") {
-                client_point_time = DateTime::parse_from_rfc3339(client_point_time_obj.as_str().unwrap()).unwrap();
-                // Use `client_point_time` here
+        let client_point = &client_point_list[i];
+        if let (Some(client_token), Some(client_time)) = (
+            client_point.get("point_token"),
+            client_point.get("point_time"),
+        ) {
+            //find the corresponding server_point
+            if let Some(server_point) = server_point_list.iter().find(|server_point| {
+                server_point.get("point_token") == Some(client_token)
+            }) {
+                // check if point_time is within latency_tolerance
+                // point times are strings in iso format
+                let client_point_time = DateTime::parse_from_rfc3339(client_time.as_str().unwrap()).unwrap();
+                let server_point_time = DateTime::parse_from_rfc3339(server_point.get("point_time").unwrap().as_str().unwrap()).unwrap();
+                let actual_latency = server_point_time.signed_duration_since(client_point_time).num_milliseconds();
+                if actual_latency.abs() > latency_tolerance {
+                    println!("Server time: {}", server_point_time);
+                    println!("Client time: {}", client_point_time);
+                    return invalid_point_response;
+                }
             } else {
-                // Handle the case where "point_time" is missing
-                eprintln!("Key 'point_time' not found in client_point");
-                return invalid_point_response;
-            }
-            server_point = server_point_obj.as_object().unwrap();
-            if let Some(server_point_time_obj) = server_point.get("point_time") {
-                DateTime::parse_from_rfc3339(server_point_time_obj.as_str().unwrap()).unwrap();
-            } else {
-                // Handle the case where "point_time" is missing
-                eprintln!("Key 'point_time' not found in server_point");
-                return invalid_point_response;
-            }
-            let actual_latency = server_point_time.signed_duration_since(client_point_time).num_milliseconds();
-            if actual_latency.abs() > latency_tolerance {
-                println!("Server time: {}", server_point_time);
-                println!("Client time: {}", client_point_time);
-                println!("Actual latency: {}", actual_latency);
+                // No match found
+                println!("No match found for client token: {}", client_token);
                 return invalid_point_response;
             }
         } else {
+            // Handle the case where "point_token" or "point_time" is missing
+            eprintln!("Key 'point_token' or 'point_time' not found in client_point");
             return invalid_point_response;
         }
     }
@@ -109,9 +99,9 @@ async fn validate_game(
     let no_game_room_response = (false, json!({"error": "No game found for user"}));
     if let Some(room_data) = game_rooms.get_mut(&user_id) {
         // check if start and end token are in room_data
-        if let Some(&GameRoomValue::String(start_game_token_server)) = room_data.get("start_game_token") {
-            if let Some(&GameRoomValue::String(end_game_token_server)) = room_data.get("end_game_token") {
-                if let Some(&GameRoomValue::List(point_list_server)) = room_data.get("point_list") {
+        if let Some(&GameRoomValue::String(ref start_game_token_server)) = room_data.get("start_game_token") {
+            if let Some(&GameRoomValue::String(ref end_game_token_server)) = room_data.get("end_game_token") {
+                if let Some(&GameRoomValue::List(ref point_list_server)) = room_data.get("point_list") {
                     if start_game_token_server == start_game_token && end_game_token_server == end_game_token{
                         // validate points
                         return validate_points(point_list_server.clone(), point_list, score).await;
@@ -153,16 +143,19 @@ async fn handle_score_update(
     state: AppState,
     body: serde_json::Value, // Adjust the type based on your expected payload
 ) -> Result<impl Reply, Rejection> {
-    // get data['score'], data['start_game_token'], data['end_game_token'], data['pointList'] from body
     let score = body["score"].as_u64().unwrap() as u32;
     let start_game_token = body["start_game_token"].as_str().unwrap();
     let end_game_token = body["end_game_token"].as_str().unwrap();
-    let user_id = body["user_id"].as_u64().unwrap() as u32;
+    let user_id = body["user_id"].as_u64().or_else(|| body["userId"].as_str()?.parse::<u64>().ok())
+    .unwrap_or_else(|| {
+        panic!("userId is missing, not an integer, or not a parseable string: {:?}", body["userId"]);
+    }) as u32;
     let point_list = body["pointList"].as_array().unwrap();
     let game_validation_result = validate_game(state.clone(), user_id, start_game_token, end_game_token, score, point_list).await;
     if !game_validation_result.0 {
         return Ok(warp::reply::json(&serde_json::json!(game_validation_result.1)));
     }
+    println!("Score was validated");
     let score = game_validation_result.1["points"].as_u64().unwrap() as u32;
     // validate the game
     // if valid, insert the score into the database using pg function, which returns a list of top10 scores and top3 personal scores
@@ -184,7 +177,7 @@ async fn handle_score_update(
     let mut top3 = Vec::with_capacity(3);
     for i in 0..high_scores.len() {
         let hs = high_scores[i].as_object().unwrap();
-        let date = DateTime::parse_from_rfc3339(&hs["score_date"].to_string()).unwrap().format("%m/%d/%Y").to_string();
+        let date = hs["score_date"].clone();
         let current_score = hs["current_score"].clone();
         let score = hs["score"].clone();
         if hs["score_type"] == "top10" {
