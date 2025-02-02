6275633cb257a8475adda6b5f338ba4f05989822
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-02-02 11:12:13 -0600

    moved game templates to games folder

--- flask_app/views.py
+++ flask_app/views.py
@@ -80,14 +80,14 @@ def game_view(game: str):
     game_info = get_game_info(game)
     if not game_info.success and type(game_info.result) == dict:
         return render_template(f"{game_info.result['type']}.html", message=game_info.result['message']), game_info.result['type']
-    game_page = game_info.result
+    game_page = game_info.result  # type: Game_Page
     if game_page.game_info.basic_circle_template: # type: ignore
         return basic_circle_template(game, game_page) # type: ignore
-    return render_template(f'{game}.html', game_page=game_page)
+    return render_template(f'/games/{game}.html', game_page=game_page)
 
 # page
 def basic_circle_template(game: str, game_page: Game_Page):
-    return render_template('basic_circle_template.html', game=game, game_page=game_page)
+    return render_template('/games/basic_circle_template.html', game=game, game_page=game_page)
 
 # page
 def auth_view(page: str):
