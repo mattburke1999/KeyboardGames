0ff15de94a1712dcaa1bb1c229d6df36e56df8d7
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-02-08 21:35:57 -0600

    adding integration and unit tests

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,23 +1,33 @@
-from app.routes import app
-from app.games.sockets import socketio
-from app.data_access.db import BaseDB
-from app.data_access.redis_store import Redis_Store
-from app.auth.routes import bp as auth_bp
-from app.games.routes import bp as games_bp
-from app.skins.routes import bp as skins_bp
+from .routes import app
+from .games.sockets import socketio
+from .data_access.db import BaseDB
+from .data_access.redis_store import Redis_Store
+from .auth.routes import bp as auth_bp
+from .games.routes import bp as games_bp
+from .skins.routes import bp as skins_bp
 from flask_cors import CORS
-from dotenv import load_dotenv
-import os
-load_dotenv(override=True)
+from config import Config
+from config import ProdConfig
+from config import TestConfig
+from config import FLASK_ENV
 
 def create_app():
-    BaseDB.initialize_pool(db_str=os.environ['DATABASE_URL'], minconn=1, maxconn=10)
-    Redis_Store.initialize(host='localhost', port=6379, password=os.getenv('REDIS_PASSWORD'))
-    
+        
     app.register_blueprint(auth_bp)
     app.register_blueprint(games_bp)
     app.register_blueprint(skins_bp)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.secret_key = Config.SECRET_KEY
+    
+    if FLASK_ENV == 'production':
+        app.config.from_object(ProdConfig)
+    else:
+        app.config.from_object(TestConfig)
+        
+    print(app.config['DB_CNXN'])
+        
+    BaseDB.initialize_pool(db_str=app.config['DB_CNXN'], minconn=1, maxconn=10)
+    Redis_Store.initialize(host='localhost', port=6379, password=app.config['DB_CNXN'])
+    
     CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
     socketio.init_app(app, cors_allowed_origins="*", async_mode='threading')
     return app, socketio
