4464a261e88ed231931e857bed9e098fa580b49f
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-02-13 21:24:42 -0600

    remove sample tests and start settin up test

--- flask_app/unit_tests/test_auth.py
+++ flask_app/unit_tests/test_auth.py
@@ -1,13 +1,14 @@
 import unittest
+from app.auth.services import create_user
+from app.auth.services import try_login
+from app.auth.services import bcrypt
 
 class TestAuth(unittest.TestCase):
     
-    
-    def test_sample(self):
-        self.assertEqual(1, 1)
-
-    def test_sample2(self):
-        self.assertEqual(2, 1)
+    def test_login_success(self):
+        # arrange
+        pass
+        
         
 if __name__ == '__main__':
     unittest.main()
