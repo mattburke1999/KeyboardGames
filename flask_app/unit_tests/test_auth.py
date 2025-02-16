75d43be566a360de0dda13c9b1219cab0ee0f246
Matthew Burke <47010776+mattburke1999@users.noreply.github.com>
2025-02-15 19:48:23 -0600

    some create user tests

--- flask_app/unit_tests/test_auth.py
+++ flask_app/unit_tests/test_auth.py
@@ -3,10 +3,17 @@ from flask import Flask
 from unittest.mock import patch
 from app.auth.services import create_user
 from app.auth.services import try_login
+from contextlib import contextmanager
 import random
 
-class Test_try_login(unittest.TestCase):
+class MockConnectionObject:
+    def commit(self):
+        pass
     
+    def rollback(self):
+        pass
+
+class AuthTest_WithSession(unittest.TestCase):
     def setUp(self):
         self.app = Flask('test')
         self.mock_session = {}
@@ -16,6 +23,20 @@ class Test_try_login(unittest.TestCase):
     
     def mock_session_set(self, key, value):
         self.mock_session[key] = value
+        
+    def mock_connection(self):
+        # return a fake object with commit() and rollback() methods
+        return MockConnectionObject()
+    
+    @contextmanager
+    def mock_connect_db(self, func):
+        yield self.mock_connection(func)
+    
+
+class Test_try_login(AuthTest_WithSession):
+    
+    def setUp(self):
+        super().setUp()
     
     @patch('app.auth.services.DB.check_user')
     @patch('app.auth.services.bcrypt.checkpw')
@@ -135,6 +156,61 @@ class Test_try_login(unittest.TestCase):
                 mock_session.__setitem__.assert_not_called()
                 mock_session.__getitem__.assert_not_called()
                 
-        
+class Test_create_user(AuthTest_WithSession):
+    
+    def setUp(self):
+        super().setUp()
+    
+    @patch('app.auth.services.DB')
+    def test_create_user_missing_fields(self, mock_DB):
+        with self.app.test_request_context():
+            with patch('app.auth.services.session') as mock_session:
+                # arrange
+                data = {'first_name': '', 'last_name': '', 'username': '', 'email': '', 'password': ''}
+                
+                # act
+                result = create_user(data)
+                
+                # assert
+                self.assertFalse(result.success)
+                self.assertIn('error', result.result)
+                self.assertEqual(result.result['error'], 'Missing required fields')
+                mock_DB.connect_db.assert_not_called()
+                mock_DB.create_user.assert_not_called()
+                mock_DB.add_default_skin.assert_not_called()
+                mock_session.__setitem__.assert_not_called()
+                mock_session.__getitem__.assert_not_called()
+    
+    @patch('app.auth.services.DB')
+    def test_create_user_success(self, mock_DB):
+        with self.app.test_request_context():
+            with patch('app.auth.services.session') as mock_session:
+                # arrange
+                data = {'first_name': 'test', 'last_name': 'test', 'username': 'test', 'email': 'test', 'password': 'test'}
+                user_id = 1
+                mock_DB.create_user.return_value = user_id
+                mock_DB.add_default_skin.return_value = None
+                mock_DB.connect_db.return_value = self.mock_connect_db
+                
+                mock_session.__getitem__.side_effect = self.mock_session_get
+                mock_session.__setitem__.side_effect = self.mock_session_set
+                
+                # act
+                result = create_user(data)
+                
+                # assert
+                if not result.success:
+                    if 'error' in result.result:
+                        print(result.result['error'])
+                    raise Exception('create_user() failed')
+                self.assertIn('registered', result.result)
+                self.assertTrue(result.result['registered'])
+                self.assertIn('logged_in', self.mock_session)
+                self.assertTrue(mock_session['logged_in'])
+                self.assertIn('user_id', self.mock_session)
+                self.assertEqual(mock_session['user_id'], user_id)
+                MockConnectionObject.commit.assert_called()
+                MockConnectionObject.rollback.assert_not_called()
+    
 if __name__ == '__main__':
     unittest.main()
